f8ed6da3ae8fab0c2f3e68c27961cfcf13128cfd
Baptiste Ory <orybaptiste@gmail.com>
2020-11-20 14:24:03 +0100

    feat: skybox v2

--- src/webgl/objects/skysphere/fragment.glsl
+++ src/webgl/objects/skysphere/fragment.glsl
@@ -7,10 +7,8 @@ varying vec3 vPosition;
 varying vec2 vUv;
 
 void main() {
-  float d = distance(vPosition, normalize(vPosition) * 0.8);
-  // vec3 color1 = vec3(1., 0., 1.);
-  // vec3 color2 = vec3(0., 0.4, 1.);
-  vec3 finalColor = mix(color1, color2, d);
+  float d = distance(vUv.y, 0.);
+  vec3 gradient = mix(color1, color2, d); 
 
-  gl_FragColor = vec4(finalColor, 1.);
+  gl_FragColor = vec4(gradient, 1.); 
 }
